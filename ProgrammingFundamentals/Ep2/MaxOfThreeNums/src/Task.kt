530942047e1ae049679b041ceae45eaad51a41be
anshuswami <anshuswamiii2004@gmail.com>
2024-04-25 00:27:08 +0530

    MaxOfThreeNumsMiniApp

--- ProgrammingFundamentals/Ep2/MaxOfThreeNums/src/Task.kt
+++ ProgrammingFundamentals/Ep2/MaxOfThreeNums/src/Task.kt
@@ -1,9 +1,14 @@
 import com.faangx.ktp.basics.MaxOfThreeNumsMiniApp
 
 fun maxOf(x: Int, y: Int, z: Int): Int {
-    TODO()
+    if (x > y && x > z) {
+        return x
+    } else if (y > x && y > z) {
+        return y
+    } else {
+        return z
+    }
 }
-
 fun main() {
     MaxOfThreeNumsMiniApp(::maxOf)
 }
