04dd04fbd81deaae9321d67f7f1ae1742f068f4b
anshuswami <anshuswamiii2004@gmail.com>
2024-04-25 01:20:37 +0530

    StageOfLifeMiniApp

--- ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
+++ ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
@@ -1,7 +1,16 @@
 import com.faangx.ktp.basics.StageOfLifeMiniApp
 
 fun stageOfLife(age: Int): String {
-    TODO()
+    if (age <= 2){
+        return "Infancy"
+    }else if (age >= 3 && age <= 11){
+        return "Childhood"
+    }else if (age >= 12 && age <= 17){
+        return "Teenage"
+    }else if (age >= 18 && age <= 59){
+        return "Adulthood"
+    }
+else{return "Old age"}
 }
 
 fun main() {
