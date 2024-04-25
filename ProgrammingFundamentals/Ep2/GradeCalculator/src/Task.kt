67fee1f913de6cd8e10bac4890a0ba9194ffc27b
anshuswami <anshuswamiii2004@gmail.com>
2024-04-25 16:18:40 +0530

    Profit Loss Calculator

--- ProgrammingFundamentals/Ep2/GradeCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep2/GradeCalculator/src/Task.kt
@@ -1,7 +1,22 @@
 import com.faangx.ktp.basics.GradeCalculatorMiniApp
 
 fun gradeFor(m1: Int, m2: Int, m3: Int, m4: Int, m5: Int): String {
-    TODO()
+    if (){
+        return "A+"
+    }else if (&&){
+        return "A"
+    }else if (&&){
+        return "B"
+    }else if (&&){
+        return "C"
+    }else if (&&){
+        return "D"
+    }else if (&&){
+        return "E"
+    }
+    else{return "Fail"}
+
+
 }
 
 fun main() {
