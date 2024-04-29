4cc95ba81572985d0525de400b5724e74fa9156c
anshuswami <anshuswamiii2004@gmail.com>
2024-04-29 22:23:00 +0530

    GreetingMiniAppv

--- ProgrammingFundamentals/Ep3/MultiplicationTable/src/Task.kt
+++ ProgrammingFundamentals/Ep3/MultiplicationTable/src/Task.kt
@@ -1,7 +1,10 @@
 import com.faangx.ktp.basics.MultiplicationTableMiniApp
+import kotlin.text.Typography.times
 
 fun printTable(num: Int) {
-    TODO()
+    repeat(10 ){
+        println("$num x ${it + 1} = ${num * (it + 1)}")
+    }
 }
 
 fun main() {
