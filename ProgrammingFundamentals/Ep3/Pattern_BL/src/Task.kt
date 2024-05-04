06d02c92491ac1181ba5f94150fe5a1e164dec28
anshuswami <anshuswamiii2004@gmail.com>
2024-05-05 00:07:11 +0530

    PatternMiniApp

--- ProgrammingFundamentals/Ep3/Pattern_BL/src/Task.kt
+++ ProgrammingFundamentals/Ep3/Pattern_BL/src/Task.kt
@@ -1,7 +1,13 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char: Char) {
-    TODO()
+    repeat (lines){
+        repeat (it + 1){
+            print(char)
+        }
+        println()
+    }
+
 }
 
 fun main() {
