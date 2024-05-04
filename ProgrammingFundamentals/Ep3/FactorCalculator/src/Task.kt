8b28f3a58028b819b022bff4e143e7baa8c37e4e
anshuswami <anshuswamiii2004@gmail.com>
2024-05-04 22:01:05 +0530

    FactorCalculatorMiniApp

--- ProgrammingFundamentals/Ep3/FactorCalculator/src/Task.kt
+++ ProgrammingFundamentals/Ep3/FactorCalculator/src/Task.kt
@@ -1,23 +1,27 @@
 import com.faangx.ktp.basics.FactorCalculatorMiniApp
 
 fun printFactorsOf(num: Int) {
-    for(i in 1 .. num ) {
-        if (num % i == 0) {
-            println("$i, ")
+    for(i in 1 .. num) {
+        if (num % i == 0){
+    print ("$i, ")
         }
     }
 }
 
 fun isPrime(num: Int): Boolean {
-    for (i in 1..num) {
-        if (num % i == 0) {
-            return true
-        } else {
-            return false
+    var p = true
+    if(num == 0 || num == 1){
+         p = false
+    }else {
+        for (i in 2..num / 2){
+            if (num % i == 0) {
+                 p = false
+            }
         }
     }
-    return true
+    return p == true
 }
+
 fun main() {
     FactorCalculatorMiniApp(::printFactorsOf, ::isPrime)
 }
