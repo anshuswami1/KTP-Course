7029434827d9f154ab3767ea24daab1981948d01
anshuswami <anshuswamiii2004@gmail.com>
2024-05-06 22:06:41 +0530

    PatternMiniApp

--- ProgrammingFundamentals/Ep3/Pattern_TRSR/src/Task.kt
+++ ProgrammingFundamentals/Ep3/Pattern_TRSR/src/Task.kt
@@ -1,13 +1,13 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int) {
-    repeat(lines) {i ->
-        repeat (i) {print(' ')}
-    for(j in lines downTo i + 1)
-        {print(j)}
+    repeat(lines){ i->
+        repeat(i){print(' ')}
+        for(j in lines downTo i + 1){
+            print(j)
+        }
         println()
-}
-
+    }
 }
 
 fun main() {
