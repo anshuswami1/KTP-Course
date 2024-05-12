0d8c6dacf688e63238057649ad00546f55d173b3
anshuswami <anshuswamiii2004@gmail.com>
2024-05-13 00:00:01 +0530

    PatternMiniApp

--- ProgrammingFundamentals/Ep3.Ext/Pattern2/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern2/src/Task.kt
@@ -1,9 +1,19 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char = '*', char2: Char = '-') {
-    TODO()
+    repeat(lines) { i ->
+        repeat(i + 1) { print(char1) }
+        repeat(lines) { print(char2) }
+        repeat(2 * (lines - i)) { print(char1) }
+        repeat(lines) { print(char2) }
+        repeat(i + 1) { print(char1) }
+
+        println()
+    }
 }
 
+
+
 fun main() {
-    PatternMiniApp("P2", ::printPattern)
+    PatternMiniApp( "P2", ::printPattern)
 }
