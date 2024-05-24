01a0ccb7ada0c5705f25a59ea5a7f36e6cd15223
anshuswami <anshuswamiii2004@gmail.com>
2024-05-24 22:37:52 +0530

    PatternMiniApp

--- ProgrammingFundamentals/Ep3.Ext/BinomialExpansion/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/BinomialExpansion/src/Task.kt
@@ -1,13 +1,29 @@
 import com.faangx.ktp.ext.BinomialExpansionMiniApp
 
 fun printBinomialExpansion(n: Int) {
-    repeat(n + 1){ i->
-    val coefficient = combinationsof(n , i)
+    repeat(n + 1){i ->
+        val cofficient = combinationsOf(n , i)
+    val powA = n - 1
+        val powB = i
+    if (cofficient > 1) print ("$cofficient")
+    if(powA > 0) {
+        print("a")
+        if (powA > 1) print("^$powA")
+    }
+        if(powB > 0){
+            print("b")
+            if (powB > 1)print("^$powB")
+        }
+        if(i < n)print (" + ")
     }
 }
-
 fun factorialOf(x: Int): Int {
-    TODO()
+    for(i in 1 .. x) {
+        if (x % i == 0){
+            print ("$i, ")
+        }
+
+}
 }
 
 fun permutationsOf(n: Int, r: Int): Int {
--- ProgrammingFundamentals/Ep3.Ext/Pattern2/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern2/src/Task.kt
@@ -1,9 +1,19 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char = '*', char2: Char = '-') {
+    repeat(lines) { i ->
+        repeat(lines) { j ->
+            if (j == lines -  i - 1) {
+                print(char1)
+            } else {
+                print(char2)
+            }
+        }
+        println()
+    }
+
 
 }
-
 fun main() {
     PatternMiniApp("P2", ::printPattern)
 }
